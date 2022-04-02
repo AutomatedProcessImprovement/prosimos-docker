eb10b176b1f82da7a6287355d15398f5e62e60b6
Iryna Halenok <iryna.halenok@gmail.com>
2022-04-02 17:37:30 +0300

    Move the certificate's generation to the production env

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -31,7 +31,7 @@ domains=(prosimos.cloud.ut.ee)
 rsa_key_size=4096
 data_path="./certbot"
 email="iryna.halenok@ut.ee" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
